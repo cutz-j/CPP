764780324f887665628184f4d97cf4b294ce42e6
Hyeonseong Jeon <cutz309@gmail.com>
2019-02-21 21:14:21 +0900

    Update

---
+++ CPP_prac/CPP_prac/ch_10.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+class Point {
+private:
+	int xpos;
+	int ypos;
+
+public:
+	Point(int x=0, int y=0): xpos(x), ypos(y) {}
+
+	void ShowPosition(void) const {
+		std::cout << "[" << xpos << ", " << ypos << "]" << std::endl;
+	}
+
+	friend Point operator- (const Point &pos1, const Point &pos2);
+	
+	Point operator+=(Point &pos2) {
+		xpos += pos2.xpos;
+		ypos += pos2.ypos;
+		return 0;
+	}
+
+	Point operator-=(Point &pos2) {
+		xpos -= pos2.xpos;
+		ypos -= pos2.ypos;
+		return 0;
+	}
+
+	friend bool operator==(Point &pos1, Point &pos2);
+	
+	friend bool operator!=(Point &pos1, Point &pos2);
+
+};
+
+Point operator- (const Point &pos1, const Point &pos2) {
+	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
+	return pos;
+}
+
+bool operator==(Point &pos1, Point &pos2) {
+	if ((pos1.xpos == pos2.xpos) && (pos1.ypos == pos2.ypos)) {
+		std::cout << "true" << std::endl;
+		return true;
+	}
+	else {
+		std::cout << "false" << std::endl;
+		return false;
+	}
+}
+
+bool operator!=(Point &pos1, Point &pos2) {
+	if (pos1 == pos2) {
+		std::cout << "false" << std::endl;
+		return false;
+	}
+	else {
+		std::cout << "true" << std::endl;
+		return true;
+	}
+}
+
+int q10_1_1(void) {
+	Point pos1 = Point(1, 2);
+	Point pos2 = Point(3, 4);
+	Point pos3 = pos2 - pos1;
+	pos3.ShowPosition();
+	return 0;
+}
+
+int q10_1_2(void) {
+	Point pos1 = Point(1, 2);
+	Point pos2 = Point(3, 4);
+	pos1 += pos2;
+	pos1.ShowPosition();
+	pos1 += pos2;
+	pos1.ShowPosition();
+	return 0;
+}
+
+int q10_1_3(void) {
+	Point pos1 = Point(1, 2);
+	Point pos2 = Point(3, 4);
+	Point pos3 = Point(1, 2);
+	pos1 == pos3;
+	pos1 != pos2;
+	return 0;
+}
+
+
+int main(void) {
+	//q10_1_1();
+	//q10_1_2();
+	q10_1_3();
+	return 0;
+}
