521507efa56b00a6312daa7a5720c286573aee40
Hyeonseong Jeon <cutz309@gmail.com>
2019-02-22 19:37:08 +0900

    Update

--- CPP_prac/CPP_prac/ch_10.cpp
+++ CPP_prac/CPP_prac/ch_10.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
 
+//namespace mystd {
+//	using namespace std;
+//	class ostream {
+//	public:
+//		ostream& operator<< (char *str) {
+//			printf("%s", str);
+//			return *this;
+//		}
+//
+//		ostream& operator<< (const char *str) {
+//			printf("%s", str);
+//			return *this;
+//		}
+//
+//		ostream& operator<< (char str) {
+//			printf("%c", str);
+//			return *this;
+//		}
+//
+//		ostream& operator<< (int num) {
+//			printf("%d", num);
+//			return *this;
+//		}
+//
+//		ostream& operator<< (double e) {
+//			printf("%g", e);
+//			return *this;
+//		}
+//
+//		ostream& operator<< (ostream& (*fp)(ostream &ostm)) {
+//			return fp(*this);
+//		}
+//
+//		ostream& operator>> (const int num) {
+//			scanf_s("%d", num, sizeof(int) * num);
+//			return *this;
+//		}
+//	};
+//
+//	ostream endl(ostream &ostm) {
+//		ostm << '\n';
+//		fflush(stdout);
+//		return ostm;
+//	}
+//
+//
+//	ostream cout;
+//	ostream cin;
+//}
+
 class Point {
 private:
 	int xpos;
@@ -26,11 +76,33 @@ public:
 		return 0;
 	}
 
+	Point operator-() {
+		xpos = -xpos;
+		ypos = -ypos;
+		return *this;
+	}
+
 	friend bool operator==(Point &pos1, Point &pos2);
 	
 	friend bool operator!=(Point &pos1, Point &pos2);
 
+	friend Point operator~(Point &pos1);
+
+	friend std::ostream& operator<<(std::ostream&, const Point&);
+
+	friend std::istream& operator>>(std::istream&, Point&);
+
 };
+
+std::ostream& operator<<(std::ostream& os, const Point& pos) {
+	os << '[' << pos.xpos << ", " << pos.ypos << ']' << std::endl;
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Point& pos) {
+	is >> pos.xpos >> pos.ypos;
+	return is;
+}
 
 Point operator- (const Point &pos1, const Point &pos2) {
 	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
@@ -57,6 +129,11 @@ bool operator!=(Point &pos1, Point &pos2) {
 		std::cout << "true" << std::endl;
 		return true;
 	}
+}
+
+Point operator~(Point &pos1) {
+	Point pos = { pos1.ypos, pos1.xpos };
+	return pos;
 }
 
 int q10_1_1(void) {
@@ -86,10 +163,29 @@ int q10_1_3(void) {
 	return 0;
 }
 
+int q10_2_1() {
+	Point pos1 = Point(1, 2);
+	Point pos3 = -pos1;
+	pos3.ShowPosition();
+	Point pos2 = ~pos1;
+	pos2.ShowPosition();
+	return 0;
+}
+
+int q10_3_1() {
+	Point pos1;
+	std::cin >> pos1;
+	std::cout << pos1;
+	return 0;
+}
+
 
 int main(void) {
 	//q10_1_1();
 	//q10_1_2();
-	q10_1_3();
+	//q10_1_3();
+	//q10_2_1();
+	//std::cout << 123 << std::endl << 3.14 << std::endl;
+	q10_3_1();
 	return 0;
 }
