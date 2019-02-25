aa660a16b18c73f21ac0c5c6eaf984d783091e93
Hyeonseong Jeon <cutz309@gmail.com>
2019-02-25 18:40:29 +0900

    Update

---
+++ CPP_prac/CPP_prac/ch_11.cpp
@@ -0,0 +1,299 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+class Person {
+private:
+	char * name;
+	int age;
+
+public:
+	Person(const char * myname, int myage) {
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy_s(name, strlen(myname) + 1, myname);
+		age = myage;
+	}
+
+	void ShowPersonInfo() const {
+		std::cout << "name: " << name << std::endl;
+		std::cout << "age: " << age << std::endl;
+	}
+
+	~Person() {
+		delete[]name;
+	}
+
+	Person& operator= (const Person& ref) {
+		delete[] name;
+		name = new char[strlen(ref.name)+1];
+		strcpy_s(name, strlen(ref.name) + 1, ref.name);
+		age = ref.age;
+		return *this;
+	}
+
+};
+
+class Gun {
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {  }
+	void Shot() {
+		std::cout << "bbang! " << bullet << std::endl;
+		bullet--;
+	}
+};
+
+class Police {
+private:
+	int handcuffs;
+	Gun * pistol;
+
+public:
+	Police(int bnum, int bcuff) : handcuffs(bcuff) {
+		if (bnum > 0)
+			pistol = new Gun(bnum);
+		else
+			pistol = NULL;
+	}
+
+	Police(const Police& ref): handcuffs(ref.handcuffs) {
+		if (ref.pistol == NULL)
+			pistol = NULL;
+		else
+			pistol = new Gun(*(ref.pistol));
+	}
+
+	Police& operator=(const Police& ref) {
+		if (pistol != NULL)
+			delete pistol;
+		if (ref.pistol != NULL)
+			pistol = new Gun(*(ref.pistol));
+		else
+			pistol = NULL;
+
+		handcuffs = ref.handcuffs;
+		return *this;
+	}
+
+	void PutHandcuff() {
+			std::cout << "snap " << handcuffs << std::endl;
+			handcuffs--;
+	}
+
+	void Shot() {
+		if (pistol == NULL)
+			std::cout << "Hut BBang!" << std::endl;
+		else
+			pistol->Shot();
+	}
+
+	~Police() {
+		if (pistol != NULL)
+			delete pistol;
+	}
+
+};
+
+int police_test() {
+	Police p1(3, 4);
+	Police p2 = p1;
+	p2.PutHandcuff();
+	p2.Shot();
+
+	Police p3(1, 2);
+	p3 = p1;
+	p3.PutHandcuff();
+	p3.Shot();
+	return 0;
+}
+
+class Book {
+private:
+	char * title;
+	char * isbn;
+	int price;
+
+public:
+	Book(const char *ti, const char * is, int pr) : price(pr) {
+		title = new char[strlen(ti) + 1];
+		strcpy_s(title, strlen(ti) + 1, ti);
+		isbn = new char[strlen(is) + 1];
+		strcpy_s(isbn, strlen(is) + 1, is);
+	}
+
+	Book(const Book& ref) : price(ref.price) {
+		title = new char[strlen(ref.title) + 1];
+		strcpy_s(title, strlen(ref.title) + 1, ref.title);
+		isbn = new char[strlen(ref.isbn) + 1];
+		strcpy_s(isbn, strlen(ref.isbn) + 1, ref.isbn);
+	}
+
+	Book& operator=(const Book& ref) {
+		price = ref.price;
+		title = new char[strlen(ref.title) + 1];
+		strcpy_s(title, strlen(ref.title) + 1, ref.title);
+		isbn = new char[strlen(ref.isbn) + 1];
+		strcpy_s(isbn, strlen(ref.isbn) + 1, ref.isbn);
+		return *this;
+	}
+
+	void ShowbookInfo() {
+		using namespace std;
+		cout << "제목: " << title << endl;
+		cout << "ISBN: " << isbn << endl;
+		cout << "가격: " << price << endl;
+	}
+
+	~Book() {
+		delete[]title;
+		delete[]isbn;
+	}
+};
+
+class EBook : Book {
+private:
+	char * DRMKey;
+public:
+	EBook(const char *ti, const char *is, int pr, const char *DRM)
+		:Book(ti, is, pr) {
+		DRMKey = new char[strlen(DRM) + 1];
+		strcpy_s(DRMKey, strlen(DRM) + 1, DRM);
+	}
+
+	EBook(const EBook& ref): Book(ref) {
+		DRMKey = new char[strlen(ref.DRMKey) + 1];
+		strcpy_s(DRMKey, strlen(ref.DRMKey) + 1, ref.DRMKey);
+	}
+
+	EBook& operator=(const EBook& ref) {
+		Book::operator=(ref); // 부모클래스 대입연산자 선언
+		delete []DRMKey; // 대입연산자의 이유
+		DRMKey = new char[strlen(ref.DRMKey) + 1];
+		strcpy_s(DRMKey, strlen(ref.DRMKey) + 1, ref.DRMKey);
+		return *this;
+	}
+
+	void ShowEBookInfo() {
+		using namespace std;
+		ShowbookInfo();
+		cout << "인증키: " << DRMKey << endl;
+	}
+};
+
+int q11_1_2() {
+	Book bk1("abc", "11", 10000);
+	Book bk2 = bk1;
+	Book bk3("def", "22", 20000);
+	bk3 = bk1;
+	bk2.ShowbookInfo();
+	bk3.ShowbookInfo();
+	
+	EBook ebk1("abc", "11", 10000, "123");
+	EBook ebk2 = ebk1;
+	EBook ebk3("def", "22", 20000, "456");
+	ebk3 = ebk1;
+	ebk2.ShowEBookInfo();
+	ebk3.ShowEBookInfo();
+	return 0;
+}
+
+class Point {
+private:
+	int xpos, ypos;
+	
+public:
+	Point(int x = 0, int y = 0) : xpos(x), ypos(y) {}
+
+	friend std::ostream& operator<<(std::ostream& os, const Point& pos);
+	friend std::ostream& operator<<(std::ostream& os, const Point* pos);
+};
+
+std::ostream& operator<<(std::ostream& os, const Point* pos) {
+	os << '[' << pos->xpos << ", " << pos->ypos << "]" << std::endl;
+	return os;
+}
+
+class BoundCheck {
+private:
+	Point**arr;
+	int arrlen;
+	BoundCheck(const BoundCheck& arr) { }
+	BoundCheck& operator=(const BoundCheck& arr) { }
+
+public:
+	BoundCheck(int len) : arrlen(len) {
+		arr = new Point*[len];
+	}
+
+	Point*& operator[] (int idx) {
+		if (idx < 0 || idx >= arrlen) {
+			std::cout << "out of bound" << std::endl;
+			exit(1);
+		}
+		return arr[idx];
+	}
+
+
+	Point* operator[] (int idx) const {
+		if (idx < 0 || idx >= arrlen) {
+			std::cout << "out of bound" << std::endl;
+			exit(1);
+		}
+		return arr[idx];
+	}
+
+	int GetArrLen() const {
+		return arrlen;
+	}
+
+	~BoundCheck() {
+		delete[]arr;
+	}
+};
+
+int q11_2_1() {
+	BoundCheck arr(3);
+	arr[0] = new Point(3, 4);
+	arr[1] = new Point(5, 6);
+	arr[2] = new Point(7, 8);
+
+	for (int i = 0; i < arr.GetArrLen(); i++) {
+		std::cout << *(arr[i]);
+		std::cout << " ";
+		std::cout << arr[i];
+		std::cout << std::endl;
+	}
+	
+	return 0;
+}
+
+int main(void) {
+	//police_test();
+	//q11_1_2();
+	q11_2_1();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
